f65c46b68d9539018a06cf886fc563d2fea0a67f
Diana Frank <diana.frank22@gmail.com>
2017-03-02 17:30:11 -0500

    finishes contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,14 +10,14 @@ export default class Contact extends Component{
   
 	render(){
 		return(
-			<div className='contact-container center'>
-			<h3 className="text-center header"> Contact Me </h3>
+			<div className='contact-container center text-center'>
+			<h3 className="header"> Contact Me </h3>
 			<h4><i>Email:</i> diana.frank22@gmail.com</h4>
       <h3 className="text-center header"> Connect </h3>
-			<h4><i>linkedin</i></h4>
-			<h4><i>Twitter</i></h4>
-			<h4><i>Github</i></h4>
-			<h4><i>bitbucket</i></h4>
+			<h4><a href="https://www.linkedin.com/in/dianafrank">LinkedIn</a></h4>
+			<h4><a href="https://twitter.com/dianafrnk">Twitter</a></h4>
+			<h4><a href="https://github.com/dianafrank22">Github</a></h4>
+			<h4><a href="https://bitbucket.org/dianafrank22">BitBucket</a></h4>
 			</div>
 		)
 	}
