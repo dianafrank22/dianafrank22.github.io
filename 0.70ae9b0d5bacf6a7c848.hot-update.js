62813d9563b79afd7c6195cbada8f8b4f5fcc2ea
Diana Frank <diana.frank22@gmail.com>
2017-03-02 18:48:44 -0500

    formats experience page

---
+++ 0.70ae9b0d5bacf6a7c848.hot-update.js
@@ -0,0 +1,244 @@
+webpackHotUpdate(0,{
+
+/***/ 240:
+/*!**************************************!*\
+  !*** ./src/components/Experience.js ***!
+  \**************************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	var _react = __webpack_require__(/*! react */ 1);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	var _reactDom = __webpack_require__(/*! react-dom */ 32);
+	
+	var _reactDom2 = _interopRequireDefault(_reactDom);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var Experience = function (_Component) {
+	  _inherits(Experience, _Component);
+	
+	  function Experience() {
+	    _classCallCheck(this, Experience);
+	
+	    var _this = _possibleConstructorReturn(this, (Experience.__proto__ || Object.getPrototypeOf(Experience)).call(this));
+	
+	    _this.state = {};
+	    return _this;
+	  }
+	
+	  _createClass(Experience, [{
+	    key: 'render',
+	    value: function render() {
+	      return _react2.default.createElement(
+	        'div',
+	        { className: 'experience center' },
+	        _react2.default.createElement(
+	          'h2',
+	          { className: 'header text-center' },
+	          'Work'
+	        ),
+	        _react2.default.createElement(
+	          'h3',
+	          null,
+	          'Komeeda'
+	        ),
+	        _react2.default.createElement(
+	          'h4',
+	          null,
+	          'Part Time Developer'
+	        ),
+	        _react2.default.createElement(
+	          'h5',
+	          null,
+	          'February 2017 - Present'
+	        ),
+	        _react2.default.createElement(
+	          'p',
+	          null,
+	          '\u2022 Collaborated with other members of the agile team to implement graphic designers designs',
+	          _react2.default.createElement('br', null),
+	          '\u2022 Built new features using Rspec tested Rails, Bootstrap, jQuery, and Sass'
+	        ),
+	        _react2.default.createElement('hr', { className: 'line-style' }),
+	        _react2.default.createElement(
+	          'h3',
+	          null,
+	          ' Nimblestack'
+	        ),
+	        _react2.default.createElement(
+	          'h4',
+	          null,
+	          ' Fullstack Developer'
+	        ),
+	        _react2.default.createElement(
+	          'h5',
+	          null,
+	          'April 2016 - October 2016'
+	        ),
+	        _react2.default.createElement(
+	          'p',
+	          null,
+	          '\u2022  Developed backend of consumer-facing AI Facebook messenger chatbot using Node.js and MongoDB',
+	          _react2.default.createElement('br', null),
+	          '\u2022 Built responsive mobile friendly frontend using React.js, Redux, ES6, CSS3, and Webpack',
+	          _react2.default.createElement('br', null),
+	          '\u2022 Modified product UX based on data from user tests and internal team discussions',
+	          _react2.default.createElement('br', null),
+	          '\u2022 Wrote test driven code using Mocha, Chai, and Sinon',
+	          _react2.default.createElement('br', null),
+	          '\u2022 Integrated Facebook, GoogleMaps, Delivery.com, and other AI APIs into products'
+	        ),
+	        _react2.default.createElement('hr', { className: 'section-line' }),
+	        _react2.default.createElement(
+	          'h2',
+	          { className: 'header text-center' },
+	          'Skills '
+	        ),
+	        _react2.default.createElement(
+	          'h3',
+	          null,
+	          'I have experience with working with some of the following... '
+	        ),
+	        _react2.default.createElement(
+	          'p',
+	          null,
+	          'React, Redux, Javascript, Mocha, Chai, Node.js, Express, Ruby, Rails, Sinatra, JSON, jQuery, SVG'
+	        ),
+	        _react2.default.createElement('hr', { className: 'section-line' }),
+	        _react2.default.createElement(
+	          'h2',
+	          { className: 'header text-center' },
+	          'Education'
+	        ),
+	        _react2.default.createElement(
+	          'h3',
+	          null,
+	          'General Assemblys WDI'
+	        ),
+	        _react2.default.createElement(
+	          'h4',
+	          null,
+	          ' November 2015 - March 2016 '
+	        ),
+	        _react2.default.createElement(
+	          'p',
+	          null,
+	          ' Full Stack Web Development Immersive (Ruby, Rails, JavaScript, jQuery, Bootstrap, and HTML5/CSS3)'
+	        ),
+	        _react2.default.createElement('hr', { className: 'line-style' }),
+	        _react2.default.createElement(
+	          'h3',
+	          null,
+	          ' Tulane University'
+	        ),
+	        _react2.default.createElement(
+	          'h4',
+	          null,
+	          ' duration '
+	        ),
+	        _react2.default.createElement(
+	          'p',
+	          null,
+	          ' description'
+	        ),
+	        _react2.default.createElement('hr', { className: 'section-line' }),
+	        _react2.default.createElement(
+	          'h2',
+	          { className: 'header text-center' },
+	          'Other'
+	        ),
+	        _react2.default.createElement(
+	          'h3',
+	          null,
+	          'BrewHacks Hackathon'
+	        ),
+	        _react2.default.createElement(
+	          'h4',
+	          null,
+	          ' April 2016'
+	        ),
+	        _react2.default.createElement(
+	          'h5',
+	          null,
+	          'Participant'
+	        ),
+	        _react2.default.createElement(
+	          'p',
+	          null,
+	          ' \u2022 First place for Brewbot, a web app & Facebook Bot that explores, discovers, and orders new types of beer',
+	          _react2.default.createElement('br', null),
+	          'Built responsive site using Bootstrap, HTML5, CSS3, and jQuery '
+	        ),
+	        _react2.default.createElement('hr', { className: 'line-style' }),
+	        _react2.default.createElement(
+	          'h3',
+	          null,
+	          'Disrupt Tech Crunch'
+	        ),
+	        _react2.default.createElement(
+	          'h4',
+	          null,
+	          ' May 2016'
+	        ),
+	        _react2.default.createElement(
+	          'h5',
+	          null,
+	          'Participant'
+	        ),
+	        _react2.default.createElement(
+	          'p',
+	          null,
+	          ' \u2022 Developed a Node.js server that read and sent email through Office365 API for Mailbot, an email assistant bot',
+	          _react2.default.createElement('br', null),
+	          '\u2022 Won prizes with Cisco for best use of cloud collaboration APIs and Microsoft for best use of Office 365 connectors'
+	        ),
+	        _react2.default.createElement('hr', { className: 'line-style' }),
+	        _react2.default.createElement(
+	          'h3',
+	          null,
+	          'speaking'
+	        ),
+	        _react2.default.createElement(
+	          'h4',
+	          null,
+	          ' date'
+	        ),
+	        _react2.default.createElement(
+	          'h5',
+	          null,
+	          'role'
+	        ),
+	        _react2.default.createElement(
+	          'p',
+	          null,
+	          ' description '
+	        )
+	      );
+	    }
+	  }]);
+	
+	  return Experience;
+	}(_react.Component);
+	
+	exports.default = Experience;
+
+/***/ }
+
+})
+//# sourceMappingURL=0.70ae9b0d5bacf6a7c848.hot-update.js.map
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -13,15 +13,21 @@ export default class Experience extends Component{
 		<div className='experience center'>
        <h2 className="header text-center">Work</h2>
             <h3>Komeeda</h3>
-            <h4>February 2017 - Present</h4>
-            <h5>Part Time Developer</h5> 
-            <p>description</p>
-                <hr className="line-style" />
-
+            <h4>Part Time Developer</h4> 
+            <h5>February 2017 - Present</h5>
+            <p>• Collaborated with other members of the agile team to implement graphic designers designs<br/>
+• Built new features using Rspec tested Rails, Bootstrap, jQuery, and Sass
+</p>
+          <hr className="line-style" />
             <h3> Nimblestack</h3>
-            <h4>April 2016 - October 2016</h4>
-            <h5> Fullstack Developer</h5>
-            <p>description</p>
+            <h4> Fullstack Developer</h4>
+            <h5>April 2016 - October 2016</h5>
+            <p>•  Developed backend of consumer-facing AI Facebook messenger chatbot using Node.js and MongoDB<br/>
+• Built responsive mobile friendly frontend using React.js, Redux, ES6, CSS3, and Webpack<br/>
+• Modified product UX based on data from user tests and internal team discussions<br/>
+• Wrote test driven code using Mocha, Chai, and Sinon<br/>
+• Integrated Facebook, GoogleMaps, Delivery.com, and other AI APIs into products 
+</p>
         <hr className="section-line" />
       <h2 className="header text-center">Skills </h2>
           <h3>I have experience with working with some of the following... </h3>
@@ -29,34 +35,31 @@ export default class Experience extends Component{
         <hr className="section-line" />
       <h2 className="header text-center">Education</h2>
           <h3>General Assemblys WDI</h3>
-          <h4> duration </h4>
-          <p> description</p>
+          <h4> November 2015 - March 2016 </h4>
+          <p> Full Stack Web Development Immersive (Ruby, Rails, JavaScript, jQuery, Bootstrap, and HTML5/CSS3)</p>
         <hr className="line-style" />
           <h3> Tulane University</h3> 
-          <h4> duration </h4>
-          <p> description</p>
+          <h4> May 2015 </h4>
+          <p> Bachelor of Science, Psychology, Gender and Sexuality </p>
     <hr className="section-line" />
-  
       <h2 className="header text-center">Other</h2>
-          <h3>hackathon - brewhacks</h3>
-          <h4> date</h4>
-          <h5>role</h5>
-          <p> description </p>
+          <h3>BrewHacks Hackathon</h3>
+          <h4> April 2016</h4>
+          <h5>Participant</h5>
+          <p> • First place for Brewbot, a web app & Facebook Bot that explores, discovers, and orders new types of beer<br/>
+Built responsive site using Bootstrap, HTML5, CSS3, and jQuery </p>
       <hr className="line-style" />
-          <h3>hackathon - disrupt tech crunch</h3>
-          <h4> date</h4>
-          <h5>role</h5>
-          <p> description </p>
+          <h3>Disrupt Tech Crunch</h3>
+          <h4> May 2016</h4>
+          <h5>Participant</h5>
+          <p> • Developed a Node.js server that read and sent email through Office365 API for Mailbot, an email assistant bot<br/>
+• Won prizes with Cisco for best use of cloud collaboration APIs and Microsoft for best use of Office 365 connectors
+ </p>
       <hr className="line-style" />
-          <h3>speaking</h3>
-          <h4> date</h4>
-          <h5>role</h5>
-          <p> description </p>
-      <hr className="line-style" />
-          <h3>hackathon - comedyhackday</h3>
-          <h4> date</h4>
-          <h5>role</h5>
-          <p> description </p>
+          <h3>General Assembly Ladies WDI Meetup</h3>
+          <h4> June 2016</h4>
+          <h5>Speaker</h5>
+          <p> Presented on post bootcamp life, participating and winning in Hackathons! </p>
     </div>
 		  
 			 
