ab70edbe4900ac3ba140ef249c71c4ef969f1b8f
Diana Frank <diana.frank22@gmail.com>
2017-03-02 18:37:06 -0500

    adds styling for experience

---
+++ 0.577317927aeff249984f.hot-update.js
@@ -0,0 +1,112 @@
+webpackHotUpdate(0,{
+
+/***/ 178:
+/*!********************************!*\
+  !*** ./src/components/Main.js ***!
+  \********************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	var _react = __webpack_require__(/*! react */ 1);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	var _reactRouter = __webpack_require__(/*! react-router */ 179);
+	
+	var _Project = __webpack_require__(/*! ./Project */ 234);
+	
+	var _Project2 = _interopRequireDefault(_Project);
+	
+	var _About = __webpack_require__(/*! ./About */ 237);
+	
+	var _About2 = _interopRequireDefault(_About);
+	
+	var _NotFound = __webpack_require__(/*! ./NotFound */ 238);
+	
+	var _NotFound2 = _interopRequireDefault(_NotFound);
+	
+	var _Contact = __webpack_require__(/*! ./Contact */ 239);
+	
+	var _Contact2 = _interopRequireDefault(_Contact);
+	
+	var _Experience = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./Experience\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	
+	var _Experience2 = _interopRequireDefault(_Experience);
+	
+	var _Nav = __webpack_require__(/*! ./Nav */ 241);
+	
+	var _Nav2 = _interopRequireDefault(_Nav);
+	
+	var _Footer = __webpack_require__(/*! ./Footer */ 242);
+	
+	var _Footer2 = _interopRequireDefault(_Footer);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var Main = function (_Component) {
+		_inherits(Main, _Component);
+	
+		function Main() {
+			_classCallCheck(this, Main);
+	
+			var _this = _possibleConstructorReturn(this, (Main.__proto__ || Object.getPrototypeOf(Main)).call(this));
+	
+			_this.state = {};
+			return _this;
+		}
+	
+		_createClass(Main, [{
+			key: 'render',
+			value: function render() {
+				return _react2.default.createElement(
+					_reactRouter.Router,
+					{ history: _reactRouter.hashHistory },
+					_react2.default.createElement(
+						_reactRouter.Route,
+						{ path: '/', component: Container },
+						_react2.default.createElement(_reactRouter.IndexRoute, { component: _About2.default }),
+						_react2.default.createElement(_reactRouter.Route, { path: '/work', component: _Project2.default }),
+						_react2.default.createElement(_reactRouter.Route, { path: '/contact', component: _Contact2.default }),
+						_react2.default.createElement(_reactRouter.Route, { path: '/experience', component: _Experience2.default }),
+						_react2.default.createElement(_reactRouter.Route, { path: '*', component: _NotFound2.default })
+					)
+				);
+			}
+		}]);
+	
+		return Main;
+	}(_react.Component);
+	
+	exports.default = Main;
+	
+	var Container = function Container(props) {
+		return _react2.default.createElement(
+			'div',
+			null,
+			_react2.default.createElement(
+				'div',
+				{ className: 'main center' },
+				_react2.default.createElement(_Nav2.default, null),
+				props.children
+			),
+			_react2.default.createElement(_Footer2.default, null)
+		);
+	};
+
+/***/ }
+
+})
+//# sourceMappingURL=0.577317927aeff249984f.hot-update.js.map
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -12,23 +12,51 @@ export default class Experience extends Component{
 		return(
 		<div className='experience center'>
        <h2 className="header text-center">Work</h2>
-         <h4>Komeeda <b>February 2017 - Present</b></h4> 
-         <p> <b> Developer Intern</b> description</p>
-         <h4>Nimblestack <b>April 2016 - October 2016</b></h4>
-         <p> <b>Fullstack Developer</b>  description</p>
-         <h4>Eden House <b> August 2014- May 2015</b></h4>
-         <p> <b>Position</b> description</p>
+            <h3>Komeeda</h3>
+            <h4>February 2017 - Present</h4>
+            <h5>Part Time Developer</h5> 
+            <p>description</p>
+                <hr className="line-style" />
+
+            <h3> Nimblestack</h3>
+            <h4>April 2016 - October 2016</h4>
+            <h5> Fullstack Developer</h5>
+            <p>description</p>
+        <hr className="section-line" />
       <h2 className="header text-center">Skills </h2>
-        <h3>I have experience with working with some of the following... </h3>
-        <p>React, Redux, Javascript, Mocha, Chai, Node.js, Express, Ruby, Rails, Sinatra, JSON, jQuery, SVG</p>
+          <h3>I have experience with working with some of the following... </h3>
+          <p>React, Redux, Javascript, Mocha, Chai, Node.js, Express, Ruby, Rails, Sinatra, JSON, jQuery, SVG</p>
+        <hr className="section-line" />
       <h2 className="header text-center">Education</h2>
-      <h5>General Assemblys WDI</h5>
-      <h5> Tulane University</h5> 
+          <h3>General Assemblys WDI</h3>
+          <h4> duration </h4>
+          <p> description</p>
+        <hr className="line-style" />
+          <h3> Tulane University</h3> 
+          <h4> duration </h4>
+          <p> description</p>
+    <hr className="section-line" />
+  
       <h2 className="header text-center">Other</h2>
-        <h4>hackathon - brewhacks</h4>
-        <h4>hackathon - disrupt tech crunch</h4>
-        <h4>speaking</h4>
-        <h4>hackathon - comedyhackday</h4>
+          <h3>hackathon - brewhacks</h3>
+          <h4> date</h4>
+          <h5>role</h5>
+          <p> description </p>
+      <hr className="line-style" />
+          <h3>hackathon - disrupt tech crunch</h3>
+          <h4> date</h4>
+          <h5>role</h5>
+          <p> description </p>
+      <hr className="line-style" />
+          <h3>speaking</h3>
+          <h4> date</h4>
+          <h5>role</h5>
+          <p> description </p>
+      <hr className="line-style" />
+          <h3>hackathon - comedyhackday</h3>
+          <h4> date</h4>
+          <h5>role</h5>
+          <p> description </p>
     </div>
 		  
 			 
