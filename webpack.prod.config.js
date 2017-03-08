11a56dfbdd55d1ff108db78f56f1cb6fdf240c32
Diana Frank <diana.frank22@gmail.com>
2017-03-08 12:12:12 -0500

    webpack production changes

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -40,7 +40,7 @@ module.exports = {
         query:{
           presets: ['es2015', 'react', "stage-0"] 
         }
-      }
+      },
         {
           test: /\.css$/,
           loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
